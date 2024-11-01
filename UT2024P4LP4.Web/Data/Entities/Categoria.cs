101250d617b8c9da2104b6a0ac5bedf007ef7be0
Héctor-Luis753 <@Hector-Luis753>
2024-11-01 15:03:42 -0400

    Agregar archivos de proyecto.

---
+++ UT2024P4LP4.Web/Data/Entities/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UT2024P4LP4.Web.Data.Dtos;
+
+namespace UT2024P4LP4.Web.Data.Entities;
+
+[Table("Categorias")]
+public class Categoria
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Nombre { get; set; } =null!;
+
+    public virtual ICollection<Producto>? Productos { get; set; }
+
+    public CategoriaDto ToDto() => new()
+    {
+        Id  = this.Id,
+        Nombre = this.Nombre,
+    };
+}
