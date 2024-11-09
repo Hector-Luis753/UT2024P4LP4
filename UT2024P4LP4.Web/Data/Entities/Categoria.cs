6c383b409a65f6bcdda06f4d626ba84398625e84
Héctor-Luis753 <@Hector-Luis753>
2024-11-09 16:44:42 -0400

    nuevo cambio

--- UT2024P4LP4.Web/Data/Entities/Categoria.cs
+++ UT2024P4LP4.Web/Data/Entities/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using UT2024P4LP4.Web.Components.Pages.Categorias;
 using UT2024P4LP4.Web.Data.Dtos;
 
 namespace UT2024P4LP4.Web.Data.Entities;
@@ -7,16 +8,33 @@ namespace UT2024P4LP4.Web.Data.Entities;
 [Table("Categorias")]
 public class Categoria
 {
-    [Key]
-    public int Id { get; set; }
-    [Required]
-    public string Nombre { get; set; } =null!;
+	[Key]
+	public int Id { get; set; }
+	[Required]
+	public string Nombre { get; set; } = null!;
 
-    public virtual ICollection<Producto>? Productos { get; set; }
+	public virtual ICollection<Producto>? Productos { get; set; }
 
-    public CategoriaDto ToDto() => new()
-    {
-        Id  = this.Id,
-        Nombre = this.Nombre,
-    };
+	public CategoriaDto ToDto() => new()
+	{
+		Id = this.Id,
+		Nombre = this.Nombre,
+	};
+
+	public static Categoria Create(int id, string nombre)
+		=> new()
+		{
+			Id = id,
+			Nombre = nombre,
+		};
+	public bool Update(string nombre)
+	{
+		var save = false;
+		if (Nombre != nombre)
+		{
+			Nombre = nombre; save = true;
+		}
+
+		return save;
+	}
 }
